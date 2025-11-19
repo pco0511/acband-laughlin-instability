fucking factor of 2 scale bug

main.py
@@ -24,6 +24,8 @@ parser = argparse.ArgumentParser(description="ED parameters")
 parser.add_argument("--n_sites", "-N", type=int, choices=[25, 27, 28], default=27, help="Number of lattice sites (N_s); must be one of {25, 27, 28}")
 parser.add_argument("--n_fermions", "-F", type=int, default=18, help="Number of fermions (N_f)")
 parser.add_argument("--K", type=float, default=0.8, help="K parameter for K_func")
+parser.add_argument("--fourier_resolution", type=int, default=256, help="Fourier resolution for AC band form factors")
+parser.add_argument("--G_radius", type=int, default=64, help="G vector radius for AC band form factors")
 args = parser.parse_args()
 
 N_s = args.n_sites
@@ -38,11 +40,11 @@ a_M = 1
 lB = ((sqrt3 / (4 * np.pi)) ** 0.5) * a_M
 
 # fourier_resolution = 128
-fourier_resolution = 256
-G_radius = 64
+fourier_resolution = args.fourier_resolution
+G_radius = args.G_radius
 V1 = 1.0
 # v1 = 3 * V1 * (a_M ** 4) / (4 * np.pi)
-v1 = sqrt3 * V1 * (a_M ** 4) / (4 * np.pi) # ????? 3 -> sqrt3
+v1 = 3 * V1 * (a_M ** 4) / (2 * np.pi) # ????? 4 pi -> 2pi
 
 
 # Lattice and Brillouin zones
@@ -158,13 +160,21 @@ plt.scatter(k_coms_flatten * lB, energies_flatten, color='red', alpha=0.25)
 plt.xlabel(r'$|\mathbf{k}_{\mathrm{COM}}|$')
 plt.ylabel('Energy')
 plt.title(f'ED Spectrum ($N={N_f}$, $N_S={N_s}$, $K={K:.3f}$)')
-os.makedirs('figs/main', exist_ok=True)
+
+
+FIG_ROOT = 'figs/supple_d'
+DATA_ROOT = 'data/supple_d'
+
+file_name = f'ed_spectrum_{N_f}_{N_s}_{K:.3f}_{fourier_resolution}_{G_radius}'
+
+os.makedirs(FIG_ROOT, exist_ok=True)
 ts = datetime.now().strftime('%Y%m%d_%H%M%S')
-plt.savefig(f'figs/main/ed_spectrum_{N_f}_{N_s}_{K:.3f}_{ts}.png', dpi=300)
+fig_path = os.path.join(FIG_ROOT, f'{file_name}_{ts}.png')
+plt.savefig(fig_path, dpi=300, transparent=True)
 plt.close()
 
-data_path = f'data/main/ed_spectrum_{N_f}_{N_s}_{K:.3f}_{ts}.pkl'
-os.makedirs('data/main', exist_ok=True)
+data_path = os.path.join(DATA_ROOT, f'{file_name}_{ts}.pkl')
+os.makedirs(DATA_ROOT, exist_ok=True)
 with open(data_path, 'wb') as f:
     pickle.dump({
         'k_coms': k_coms_flatten,

src/acband.py
@@ -20,7 +20,8 @@ def K_func1(
         args: tuple[float, np.ndarray, np.ndarray, np.ndarray]
     ) -> np.ndarray:
     K, b1, b2, b3 = args
-    coeff = -np.sqrt(3) / (4 * np.pi) * K
+    # coeff = -np.sqrt(3) / (4 * np.pi) * K
+    coeff = -np.sqrt(3) / (2 * np.pi) * K # ????? 4 pi -> 2 pi
     cos1 = np.cos(einsum(x, b1, "... d, d -> ..."))
     cos2 = np.cos(einsum(x, b2, "... d, d -> ..."))
     cos3 = np.cos(einsum(x, b3, "... d, d -> ..."))
@@ -210,8 +211,8 @@ def interaction_matrix(
         k3 = p
         k4 = k
 
-        k1_coord = bz.k_coords[k1]
-        k2_coord = bz.k_coords[k2]
+        # k1_coord = bz.k_coords[k1]
+        # k2_coord = bz.k_coords[k2]
         k3_coord = bz.k_coords[k3]
         k4_coord = bz.k_coords[k4]
         q_coord = bz.k_coords[q]
@@ -224,10 +225,10 @@ def interaction_matrix(
         V_grid = V(q_vec_shited_grid)
 
         # for Lambda 1:
-        l1_start_x = start_idx + G1_coord[0]
-        l1_end_x   = end_idx   + G1_coord[0]
-        l1_start_y = start_idx + G1_coord[1]
-        l1_end_y   = end_idx   + G1_coord[1]
+        l1_start_x = start_idx - G1_coord[0]
+        l1_end_x   = end_idx   - G1_coord[0]
+        l1_start_y = start_idx - G1_coord[1]
+        l1_end_y   = end_idx   - G1_coord[1]
 
         # for Lambda 2:
         l2_start_x = start_idx - G2_coord[0]